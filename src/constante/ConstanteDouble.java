9c7f1cd45a4c6027d7d222cdc9b3599d746330e6
Corre-Morgan <morgan.corre@etu.univ-amu.fr>
2019-10-15 09:09:07 +0200

    mise en place de valeur pour rendre plus faible les monstres ennemis

--- src/constante/ConstanteDouble.java
+++ src/constante/ConstanteDouble.java
@@ -5,7 +5,11 @@ public enum ConstanteDouble {
     DROP_RATE_RANK2 (0.37),
     DROP_RATE_RANK3 (0.17),
     DROP_RATE_RANK4 (0.07),
-    DROP_RATE_RANK5 (0.02);
+    DROP_RATE_RANK5 (0.02),
+
+    MOBS_WEAK(0.5),
+    MOBS_NORMAL(1.00),
+    MOBS_HARD(1.5);
 
     private double valeur;
 
